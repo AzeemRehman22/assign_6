442eaa964b19beab6e6c678864515f30569fe7f5
AzeemRehman22 <64614886+AzeemRehman22@users.noreply.github.com>
2020-06-24 20:37:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,381 @@
+
+//*****Cheapetr No 21 - 25 Task # 1
+
+1.//var a = prompt("Enter The First Nmae")
+//var b = prompt("Enter The Last Name ")
+
+//var c = a + b;
+//document.write("Full_Name: ",c)
+
+//*****Cheapetr No 21 - 25 Task # 2
+
+2.//var a = prompt("Enter The Your Favorit Mobile Phone Model")
+
+//document.write("Length Of String: ",a.length)
+
+//*****Cheapetr No 21 - 25 Task # 3
+
+3.//var a = "Pakistani"
+//document.write("Index Of 'n': ",a.indexOf("n"))
+
+//*****Cheapetr No 21 - 25 Task # 4
+
+4.//var a = "Hello World"
+//document.write("Last Index Of 'l': ",a.lastIndexOf("l"))
+
+//*****Cheapetr No 21 - 25 Task # 5
+
+5.//var a = "Pakistani";
+//document.write("Character At Index 3: ",a.charAt(3))
+
+//*****Cheapetr No 21 - 25 Task # 6
+
+6.//var First_Name = "Azeem";
+//var Last_Name = "Rehman";
+//document.write(First_Name + Last_Name);
+
+//*****Cheapetr No 21 - 25 Task # 7
+
+7.//var City = "City Islamabad";
+//var change  =  City.replace("Islamabad", "Hyderabad");
+//document.write(change);
+
+//*****Cheapetr No 21 - 25 Task # 8
+
+8.//var para = "Ali and Sami are best friends. They play cricket and football together.";
+//var para1 = para.replace(/and/g, "&");
+//document.write(para1);
+
+//*****Cheapetr No 21 - 25 Task # 10
+
+10.//var a = "peanutrs";
+//var b = a.toUpperCase();
+//document.write(b); 
+
+//*****Cheapetr No 21 - 25 Task # 11
+
+11.//var a = prompt("Enter the value");
+//var b = a.toUpperCase();
+//document.write(b);
+
+//*****Cheapetr No 21 - 25 Task # 12
+
+12.//var a = parseInt("472")
+ //document.write("Value:",a);
+
+ //*****Cheapetr No 21 - 25 Task # 13
+ 
+13.//var num = 35.36;
+ //var round = num.toString();
+ //document.write(round);
+
+//*****Cheapetr No 21 - 25 Task # 14
+
+14.//  var UserName = prompt("Enter the UserName: ")
+// if (UserName =="Azeem_rehman"){
+//     alert("You are Successully Login")
+// }
+// else{
+//     alert("Please Enetr the valid UserName")
+// }
+
+//*****Cheapetr No 21 - 25 Task # 15
+
+15.// var bakery = prompt("enter the item");
+
+//     if(bakery === "cake" & "Apple pie" & "cookies" & "chips" & "patties"){
+//         alert(bakery + " is available in our bakery")
+//     }
+//     else{
+//         alert(bakery + " we are sorry. this item is not in our bakery")
+//     }
+
+//*****Cheapetr No 21 - 25 Task # 16
+
+16.// var pass = prompt("Enter the password")
+// if(pass === num ){
+//     alert(pass + " your are successfully login")
+// }
+// else{
+//     alert(pass+ "Enter a valid password")
+// }
+
+//*****Cheapetr No 21 - 25 Task # 17
+
+17.// var university = "University Of Karachi";
+
+// var name = university.split('')
+
+// document.write(name);
+
+//*****Cheapetr No 21 - 25 Task # 18
+
+18.// var user = prompt("Enter The UserName: ")
+// var user1 = user.charAt("n");
+// document.write(user1);
+
+//*****Cheapetr No 26 - 30 Task # 1
+
+1.// var a = 3.45214;
+// document.write("Number: "+a )
+
+// var b = Math.round(a);
+// document.write("Round Of Value: "+b);
+
+// var c = Math.floor(a);
+// document.write("Floor Value: "+c);
+
+// var d = Math.ceil(a);
+// document.write("Ceil Value: "+ a);
+
+//*****Cheapetr No 26 - 30 Task # 2
+
+2.// var a = -2.6733;
+// document.write("Number: " + a);
+
+// var b = Math.round(a);
+// document.write("Round of Value: " + b);
+
+// var c = Math.floor(a);
+// document.write("Floor Value: " + c);
+
+// var d = Math.ceil(a);
+// document.write("Ceil Value: " + d);
+
+//*****Cheapetr No 26 - 30 Task # 3
+
+3.// var a = Math.abs(-4);
+// document.write("The Absolute Value Of -4 is: " + a);
+
+//*****Cheapetr No 26 - 30 Task # 4
+
+4.// var a = Math.floor(Math.random()*10);
+// document.write("Ramdom Dice Value: "+a);
+
+//*****Cheapetr No 26 - 30 Task # 5
+
+5.// var headuser = prompt("Enter the head Username: ");
+// var tailuser = prompt("Enter Tails username: ");
+
+//*****Cheapetr No 26 - 30 Task # 6
+
+6.// var a = Math.floor(Math.random()*100);
+// document.write("Random Number Between 1 to 100: "+ a);
+
+//*****Cheapetr No 26 - 30 Task # 8
+
+8.// var a = prompt("Enter the number between 1 and 10 ");
+
+// var b = Math.floor(Math.random(a)*10);
+// if (b===a){
+//     alert("Congratulat you are login"+ b);
+// }else{
+//     alert("try again!"+ b);
+// }
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// ***** chepter No 31 - 34 Task # 1
+
+1.// var a = new Date();
+// document.write(a);
+
+// ***** chepter No 31 - 34 Task # 2
+
+2.// var a = new Date();
+// var b = a.toString();
+// var c = b.slice(0,8);
+// document.write("Current Month: " + c);
+
+// ***** chepter No 31 - 34 Task # 3
+
+3.// var a = new Date();
+// var b = a.toString();
+// var c = b.slice(0,3);
+// document.write("Today is: " + c);
+
+// ***** chepter No 31 - 34 Task # 4
+
+4.// var a = new Date();
+// var day = ["Mon","Thues","Web","Thur","Fri","Stur","Sun"];
+// c = day[a.getDay()];
+
+// if(c === Stur && Sun){
+//     document.write("Its Funday"+c);
+// }else{
+//     document.write("Today Is" + c);
+// }
+
+// ***** chepter No 31 - 34 Task # 6
+
+6.// var a = new Date();
+// var dob = new Date("jan 1, 1970");
+// var dobmili = dob.getTime();
+// var dobminute = dob.getMinutes();
+// document.write("Current Date: " +a+"<br>","Elapsed milisecond since jan 1, 1970:"+dobmili+"<br>"," Elapsed minuts since jan 1, 1970: "+dobminute);
+
+// ***** chepter No 31 - 34 Task # 7
+
+7.// function formatAMPM(date) {
+//     var hours = date.getHours();
+//     var minutes = date.getMinutes();
+//     var ampm = hours >= 12 ? 'pm' : 'am';
+//     hours = hours % 12;
+//     hours = hours ? hours : 12; // the hour '0' should be '12'
+//     minutes = minutes < 10 ? '0'+minutes : minutes;
+//     var strTime = hours + ':' + minutes + ' ' + ampm;
+//     return strTime;
+//   }
+  
+//   document.write(formatAMPM(new Date));
+
+// ***** chepter No 31 - 34 Task # 8
+
+8.// var date = new Date();
+// var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+// var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+// document.write("Later Date : "+lastDay)
+
+// ***** chepter No 31 - 34 Task # 9
+ 
+9.//  var date1 = new Date("04/24/2020"); 
+//  var date2 = new Date("06/24/2020");  
+//  var Difference_In_Time = date2.getTime() - date1.getTime(); 
+//  var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24); 
+//  document.write(Difference_In_Days + " days have pass since 1st Ramdan,2020"); 
+
+// ***** chepter No 31 - 34 Task # 12
+
+12.// var date = new Date();
+// var intYear = date.getFullYear() - 100;
+// document.write("Current Date: "+date+"<br>","after 100 year back: "+intYear);
+
+// ***** chepter No 31 - 34 Task # 13
+
+13.// var dob = new Date(prompt("Enter your date of birth"));
+// var dobmili = dob.getTime();
+// var today = new Date();
+// var todaymili = today.getTime();
+// var diff = todaymili - dobmili;
+// var accuage = Math.floor(diff/(1000*60*60*24*30*12))
+// document.write("Your Age is: " + accuage);
+
+// ***** chepter No 31 - 34 Task # 14
+
+14.// var head = "K_ELECTRIC BILL";
+// var a = prompt("Enetr The Customer Name: ");
+// var b = prompt("Enetr the Current Monthy");
+// var c = prompt("enter the Number of unit: ");
+// var d = prompt("Enter the charges of unit: ");
+// var e = c*d;
+// var f = prompt("enetr the late paymint: ");
+// var g = +e + +f;
+// document.write(head +"<br>","<br>","Customer_Name: "+ a+ "<br>","Current Month: "+b+"<br>","Number Of Unit: "+c+"<br>"
+// ,"Charges of Unit: "+d+"<br>","<br>","Net Amount of Paybale(with due date): "+e+"<br>","Late Paymint Surcharges: "+f+"<br>","Gross Amount Payable(After Due Date): " +g);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// **** Cheapter No 35 - 38 Task # 1
+
+1.// function date(){
+//     var a = new Date();
+//     document.write(a);
+// }
+// date();
+
+// **** Cheapter No 35 - 38 Task # 2
+
+
+2.// function name (){
+//     var name1 = prompt("Enter the Firstnam");
+//     var name2 = prompt("Enter the Lastname");
+//     var fname = name1 + name2
+//     document.write("Firstanme: "+name1+ "<br>","Lastname: "+name2+"<br>","FullName:"+fname);
+// }
+// name ();
+
+// **** Cheapter No 35 - 38 Task # 3
+
+3.// function add(){
+// var num1 = prompt("Enter the firstnum");
+// var num2 = prompt("Enter the secondnum");
+// var sum = +num1 + +num2
+// document.write("FisrtNumber: "+num1+"<br>","SecondNumber: "+num2+"<br>","Total_Sum: "+sum);
+
+// }
+
+// add();
+
+// **** Cheapter No 35 - 38 Task # 4
+
+4.// function  caculator(){
+//     var num1 = prompt("Enter the num1");
+//     var Operator = prompt("Enter the operator");
+//     var num2 = prompt("Enter the num2");
+//     var head = "CALCULATOR";
+//     document.write(head,"<br>",num1 + Operator + num2,":");
+//     if(Operator === '+'){
+//         document.write(+num1 + +num2);
+//     }
+//     else if(Operator === '-'){
+//         document.write(num1 - num2);
+//     }
+//     else if (Operator === '*'){
+//         document.write(num1 * num2);
+//     }
+//     else if (Operator === '/'){
+//         document.write(num1 / num2);
+//     }
+// }
+// caculator();
+
+// **** Cheapter No 35 - 38 Task # 5
+
+5.// function square(){
+//     var value = prompt("Enter the value");
+//     var b = Math.sqrt(value);
+//     document.write(b);
+// }
+// square();
+
+// **** Cheapter No 35 - 38 Task # 6
+
+6.// function fact(){
+
+// var x = prompt("Enter the value");
+// if (x === 0) {  
+//     return 1;  
+// } 
+// else {  
+//     return x * fact(x - 1);  
+// }
+// console.log(fact(x));
+// }
+// fact();
+
+// **** Cheapter No 35 - 38 Task # 9
+
+// function area() {
+//     var a = prompt("Enter the wieght");
+//     var b = prompt("Enter the height");
+//     var Area = a * b
+//     document.write("width :" +a+ "<br>","height :"+b+"<br>","Area Of the rectangle:"+Area)
+    
+// }
+// area();
+
+
+function palindrome() {
+    var wrd = prompt("Enter the word");
+    var check = "";
+    for(var i =0;wrd.length - 1; i--){
+        check += wrd[i];
+
+    }
+    if(check === wrd){
+        document.write(wrd + "Is palindrome word")
+    }
+    else{
+        document.write(wrd + "is not a palindrame word")
+    }
+    
+}
+palindrome();
+
